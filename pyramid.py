67a375d1cf2736bf4fe3c5616d63dec0ba593d5b
Srijan-5 <144520900+Srijan-5@users.noreply.github.com>
2024-05-24 23:22:58 +0530

    Add files via upload

---
+++ pyramid.py
@@ -0,0 +1,3 @@
+n = int(input("Enter the number of rows for the pyramid pattern: "))
+for i in range(1, n + 1):
+    print(" " * (n - i) + "*" * (2 * i - 1))
---
+++ upper triangle.py
@@ -0,0 +1,3 @@
+n = int(input("Enter the number of rows for the upper triangular pattern: "))
+for i in range(n, 0, -1):
+    print("*" * i)
